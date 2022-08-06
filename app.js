5080045c209bc5a35920dabe1f2555574c534c62
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-08-06 10:53:29 -0600

    Authentication added for category update and delete

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-// Test comment....
+var dotenv = require('dotenv').config();
 
 // Routes
 var indexRouter = require('./routes/index');
